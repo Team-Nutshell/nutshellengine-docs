23e463cee496e7f39420df12b8aaeedd65299e7f
ntsh-oni <teamnutshell.dev@gmail.com>
2024-10-05 22:01:23 +0200

    version 0.1.2

--- source/conf.py
+++ source/conf.py
@@ -9,8 +9,8 @@
 project = 'NutshellEngine'
 copyright = '2022-2024, Team Nutshell'
 author = 'Team Nutshell'
-release = '0.1.1'
-version = '0.1.1'
+release = '0.1.2'
+version = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
